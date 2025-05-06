3b5a96e9b306c13ba482c28098f5e841ebc72d74
Gavin Rhoades <84148006+SithLor@users.noreply.github.com>
2025-05-06 16:56:39 +0000

    Add pull script and update repository list in README.md

---
+++ pull.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd ./packages 
+
+# Clone all repositories listed in README.md
+while read -r repo; do
+  if [[ $repo == https://github.com/* ]]; then
+    git clone "$repo"
+  fi
+done < repo_list.txt
