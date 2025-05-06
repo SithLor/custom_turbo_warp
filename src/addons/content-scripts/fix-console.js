2ea68e1125af9ca56c78afaccef9f2192fefc34e
Gavin Rhoades <84148006+SithLor@users.noreply.github.com>
2025-05-06 20:37:32 +0000

    dsadasd

--- src/addons/content-scripts/fix-console.js
+++
@@ -1,9 +0,0 @@
-function fixConsole() {
-  window._realConsole = {
-    ...console,
-  };
-}
-
-if (!(document.documentElement instanceof SVGElement)) {
-  immediatelyRunFunctionInMainWorld(fixConsole);
-}
--- src/json/rollup.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  input: './src/index.js',
-  output: {
-    file: './lib/cjs.cjs',
-    format: 'cjs'
-  }
-};
